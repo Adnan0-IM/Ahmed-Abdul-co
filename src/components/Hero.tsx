07996b4a439ce9855790c6aeafff1171ac099e32
Adnan0-IM <adnanilyas969@gmail.com>
2025-07-11 00:55:35 +0100

    fix: Update text sizes in Hero component for improved readability

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,13 +15,13 @@ const Hero = ({ title, title2, content, buttonText }: heroProps) => {
         <div className="container mx-auto px-4 lg:px-8 ">
           <div className="flex flex-col text-center items-center md:items-start md:text-start gap-6 py-24  md:w-1/2 text-white">
             {title && (
-              <h1 className="text-xl sm:text-3xl  font-bold">{title}</h1>
+              <h1 className="text-2xl sm:text-3xl  font-bold">{title}</h1>
             )}
-            {title2 && <h2 className="text-xl sm:text-2xl font-bold">{title2}</h2>}
+            {title2 && <h2 className="text-2xl sm:text-2xl font-bold">{title2}</h2>}
             {content && <p className="text-base sm:text-lg">{content}</p>}
             {buttonText && (
               <Link
-                className="bg-primary-light text-base mt-4 md:mt-0 shadow-md hover:scale-105 w-full md:w-1/2 transition-all duration-200 hover:bg-primary-dark text-center font-medium p-3 md:p-4 rounded-md  "
+                className="bg-primary-light text-base mt-4 md:mt-0 shadow-md hover:scale-105 w-full md:w-1/2 transition-all duration-200 hover:bg-primary-dark text-center font-medium p-4 rounded-md  "
                 to="/services"
               >
                 {buttonText}
